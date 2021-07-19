c19b10d86f2ce3bb9a4bb0a4825ac5955dba8f15
Damian Ly <damian@rayatheapp.com>
2021-07-18 18:24:16 -0700

    updating naming to community feedback platform

--- lib/sample/databases/pg-schema.sql
+++ lib/sample/databases/pg-schema.sql
@@ -1,14 +1,14 @@
 -- Seeing as we will be testing out this script alot we can destroy the db before creating everything again
-DROP DATABASE IF EXISTS translationsdb;
+DROP DATABASE IF EXISTS community-feedback-db;
 
 -- Create the db
-CREATE DATABASE translationsdb;
+CREATE DATABASE community-feedback-db;
 
 -- Move into the db
-\c translationsdb
+\c community-feedback-db
 
 -- Create our table if it doesn't already exist
-CREATE TABLE IF NOT EXISTS Translations
+CREATE TABLE IF NOT EXISTS Sample
 (
     key character varying(100),
     lang character varying(5),
@@ -16,5 +16,5 @@ CREATE TABLE IF NOT EXISTS Translations
 );
 
 -- Changes the owner of the table to postgres which is the default when installing postgres
-ALTER TABLE Translations
+ALTER TABLE Sample
     OWNER to postgres;
